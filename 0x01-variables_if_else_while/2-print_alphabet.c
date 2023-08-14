b5e840243c33c9990baa753fe1c799e434e4bc40
Mennarefat <mennarefaat22@gmail.com>
2023-08-14 03:14:47 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+/**
+*main-Entry point
+*
+*Return:0(success)
+*/
+void main(void)
+{
+char ch = 'a';
+while (ch <= 'z')
+{
+putchar(ch);
+ch++;
+}
+putchar("\n");
+return (0);
+}
