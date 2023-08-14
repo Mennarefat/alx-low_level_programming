ac84fbfc838b8d8568232c064165b436f44ad6c6
Mennarefat <mennarefaat22@gmail.com>
2023-08-14 05:46:49 -0700

    print comb

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+/**
+*main-Entry point
+*
+*Return:0(success)
+*/
+int main(void)
+{
+int fd = 0, sd;
+while (fd <= 99)
+{
+sd = fd;
+while (sd <= 99)
+{
+if (sd != fd)
+{
+putchar((fd / 10) + 48);
+putchar((fd % 10) + 48);
+putchar(' ');
+putchar((sd / 10) + 48);
+putchar((sd / 10) + 48);
+if (fd != 98 || sd != 99)
+{
+putchar(',');
+putchar(' ');
+}
+}
+sd++;
+}
+fd++;
+}
+putchar('\n');
+return (0);
+}
+
