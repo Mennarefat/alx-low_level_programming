3b9b9977c712934bb5695441ae5b0e99197fd645
Mennarefat <mennarefaat22@gmail.com>
2023-08-16 05:22:20 -0700

    main

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,20 @@
+#include"main.h"
+/**
+*jack_bauer-Prints every minute of the day. 
+*/
+void jack_bauer(void)
+{
+int min, H;
+for (H = 0; H <= 23; H++)
+{
+for (min = 0; min <= 59; min++)
+{
+_putchar((H / 10) + 48);
+_putchar((H % 10) + 48);
+_putchar(':');
+_putchar((min / 10) + 48);
+_putchar((min % 10) + 48);
+_putchar('\n');
+}
+}
+}
