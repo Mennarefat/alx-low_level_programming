00badb9dae70504df5cc704a7cb715680f3a1a50
Mennarefat <mennarefaat22@gmail.com>
2023-09-02 10:19:04 -0700

    libmy

---
+++ 0x09-static_libraries/0-strcat.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+*_strcat - concatenates two strings
+*@dest: input value
+*@src: input value
+*
+*Return: void
+*/
+char *_strcat(char *dest, char *src)
+{
+int i = 0;
+int j;
+while (dest[i] != '\0')
+i++;
+for (j = 0; src[j]; j++)
+dest[i++] = src[j];
+return (dest);
+}
+
